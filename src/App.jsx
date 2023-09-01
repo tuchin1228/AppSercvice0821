ef4b8de3092fe07a4a1faca0ce659df95bd69ab1
tuchin1228 <jhinwei1228@gmail.com>
2023-09-01 10:30:41 +0800

    第三方登入測試

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import axios from "axios";
 import "./App.css";
+import Login from './Login'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -62,6 +63,8 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+
+      <Login />
     </>
   );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      Login
+      <div><a href="/.auth/login/google">Log in with Google</a></div>
+    </div>
+  );
+}
+
+export default Login;
