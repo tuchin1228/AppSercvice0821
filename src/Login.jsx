6419afc1ebc4025143e71fb3bd50823021d52b28
tuchin1228 <jhinwei1228@gmail.com>
2023-09-01 15:20:19 +0800

    google login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { GoogleOAuthProvider, GoogleLogin, useGoogleLogin, useGoogleOneTapLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
 
 function Login() {
   return (
     <div>
       Login
-      <div><a href="/.auth/login/google">Log in with Google</a></div>
+      <div>
+        <GoogleOAuthProvider clientId="899005079900-f0n83l0hmt7ip7go3c4qc6ds5q34k3nt.apps.googleusercontent.com">
+          <GoogleLogin
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse.credential);
+              var decoded = jwt_decode(credentialResponse.credential);
+              console.log(decoded);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+          />
+        </GoogleOAuthProvider>
+      </div>
     </div>
   );
 }
